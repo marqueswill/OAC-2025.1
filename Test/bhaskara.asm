8300ff3f6fef4a3628cfcbd3b80693177a32ead5
Willyan Marques <willyanmarquesmelo@gmail.com>
2025-05-11 21:13:11 -0300

    calculate real

--- Test/bhaskara.asm
+++ Test/bhaskara.asm
@@ -35,26 +35,55 @@ read_input:
 
 # fa0 = a, fa1 = b, fa2 = c
 bhaskara: 
-	addi sp, sp, -12
+	addi sp, sp, -20
 	sw ra, 0(sp)
-	sw s0, 4(sp) # x1
-	sw s1, 8(sp) # x2
+	fsw fs0, 4(sp)  # a
+	fsw fs1, 8(sp)  # b
+	fsw fs2, 12(sp) # c
+	fsw fs3, 16(sp) # x1
+	fsw fs4, 20(sp) # x2
+	
+	fmv.s fs0, fa0
+	fmv.s fs1, fa1
+	fmv.s fs2, fa2
 
-	jal bhaskara_delta
+	jal bhaskara_delta # fa0 = delta
 
-	li t0, 4
-	fcvt.s.w ft1, t0
-	flt.s t0, fa0, ft1
-	bnez t0, bhaskara_complexas
+	li t0, 0
+	fcvt.s.w ft0, t0
+	flt.s t0, fa0, ft0
+	bnez t0, bhaskara_complexas # delta < 0
+
 
 	bhaskara_reais:
-		li a0, 1
 		# todo: calcular raízes reais
+		fsqrt.s ft0, fa0  # ft0 = sqrt(delta)
+		
+		li t0, 2
+		fcvt.s.w ft1, t0
+		fmul.s  ft1, ft1, fs0 # ft1 = 2a 
+		
+		li a7, 2
+
+		# raiz x1
+		fneg.s fs3, fs1 # fs4 = -b
+		fsub.s fs3, fs3, ft0 # -b - sqrt(delta)
+		fdiv.s fs3, fs3, ft1 # fs3 = (-b + sqrt(delta))/2a
+
+		# raiz x2
+		fneg.s fs4, fs1  # fs3 = -b
+		fadd.s fs4, fs4, ft0 # fs3 = -b + sqrt(delta)
+		fdiv.s fs4, fs4, ft1 # fs3 = (-b + sqrt(delta))/2a
+
+
+
+		li a0, 1
 		j bhaskara_end
 
 	bhaskara_complexas:
-		li a0, 2
 		# todo: calcular raízes complexas
+	
+		li a0, 2
 		j bhaskara_end
 
 	# todo: descobrir em que casos ocorre o erro
@@ -63,20 +92,23 @@ bhaskara:
 		j bhaskara_end
 
 	bhaskara_end:
-		mv s0, t0
-		mv s1, t1
-		
+		fmv.s ft0, fs3
+		fmv.s ft1, fs4
+
 		lw ra, 0(sp)
-		lw s0, 8(sp)
-		lw s1, 4(sp)
-		addi sp, sp, 12
+		flw fs0, 4(sp)
+		flw fs1, 8(sp)
+		flw fs2, 12(sp)
+		flw fs3, 16(sp)
+		flw fs4, 20(sp)
+		addi sp, sp, 20
 		
-		sw t0, 0(sp)
-		sw t1, 4(sp)
+		fsw ft0, 0(sp)
+		fsw ft1, 4(sp)
 
 		ret
 
-	# a0 = [a, b, c]
+	# fa0 = a, fa1 = b, fa2 = c
 	bhaskara_delta:
 		fmul.s ft0, fa1, fa1 # b^2
 		
