37624bfca196f7ee74ff688a29cfd189a4cd5d66
Willyan Marques <willyanmarquesmelo@gmail.com>
2025-05-18 19:11:58 -0300

    Changes to be committed: 	new file:   "Q2/2.4/anota\303\247\303\265es.txt" 	deleted:    Q2/2.4/image.png 	new file:   Q2/2.4/status_O1.png 	new file:   Q2/2.4/status_O2.png 	modified:   Q2/2.4/time_test.asm

--- Q2/2.4/time_test.asm
+++ Q2/2.4/time_test.asm
@@ -1,111 +1,137 @@
 main:
-	csrr s1,3074   # le o num instr atual 
-	csrr s0,3073   # le o time atual 
-	
-	jal ff1
-	
-	csrr t0,3073   # le o time atual 
-	csrr t1,3074   # le o num instr atual 
-	
-	li a7, 1
-	sub a0,t0,s0   # calcula o tempo de execução Texec em ms 
-	ecall
-	sub a0,t1,s1
-	ecall
-	li a0, '\n'
-	li a7, 11
-	ecall
-	ecall
+csrr s1,3074
+csrr s0,3073
+call ff1
+csrr t0,3073    
+csrr t1,3074   
+sub s0,t0,s0    
+sub s1,t1,s1   
 
-	csrr s1,3074   # le o num instr atual 
-	csrr s0,3073   # le o time atual 
-	
-	jal ff2
-	
-	csrr t0,3073   # le o time atual 
-	csrr t1,3074   # le o num instr atual 
-	
-	li a7, 1
-	sub a0,t0,s0   # calcula o tempo de execução Texec em ms 
-	ecall
-	sub a0,t1,s1
-	ecall
-	li a0, '\n'
-	li a7, 11
-	ecall
-	ecall
-	
-	csrr s1,3074   # le o num instr atual 
-	csrr s0,3073   # le o time atual 
-	
-	jal ff3
-	
-	csrr t0,3073   # le o time atual 
-	csrr t1,3074   # le o num instr atual 
-	
-	li a7, 1
-	sub a0,t0,s0   # calcula o tempo de execução Texec em ms 
-	ecall
-	sub a0,t1,s1
-	ecall
-	li a0, '\n'
-	li a7, 11
-	ecall
-	ecall
-	
-	csrr s1,3074   # le o num instr atual 
-	csrr s0,3073   # le o time atual 
-	
-	jal ff4
-	
-	csrr t0,3073   # le o time atual 
-	csrr t1,3074   # le o num instr atual 
-	
-	li a7, 1
-	sub a0,t0,s0   # calcula o tempo de execução Texec em ms 
-	ecall
-	sub a0,t1,s1
-	ecall
-	li a0, '\n'
-	li a7, 11
-	ecall
-	ecall
-	
-	csrr s1,3074   # le o num instr atual 
-	csrr s0,3073   # le o time atual 
-	
-	jal ff5
-	
-	csrr t0,3073   # le o time atual 
-	csrr t1,3074   # le o num instr atual 
-	
-	li a7, 1
-	sub a0,t0,s0   # calcula o tempo de execução Texec em ms 
-	ecall
-	sub a0,t1,s1
-	ecall
-	li a0, '\n'
-	li a7, 11
-	ecall
-	ecall
+li a7, 1
+mv a0, s0
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+li a7, 1
+mv a0, s1
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+ecall
 
-	csrr s1,3074   # le o num instr atual 
-	csrr s0,3073   # le o time atual 
-	
-	jal ff6
-	
-	csrr t0,3073   # le o time atual 
-	csrr t1,3074   # le o num instr atual 
-	
-	li a7, 1
-	sub a0,t0,s0   # calcula o tempo de execução Texec em ms 
-	ecall
-	sub a0,t1,s1
-	ecall
-	li a0, '\n'
-	li a7, 11
-	ecall
-	ecall
+
+csrr s1,3074
+csrr s0,3073
+call ff2
+csrr t0,3073    
+csrr t1,3074   
+sub s0,t0,s0    
+sub s1,t1,s1   
+
+li a7, 1
+mv a0, s0
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+li a7, 1
+mv a0, s1
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+ecall
+
+
+csrr s1,3074
+csrr s0,3073
+call ff3
+csrr t0,3073    
+csrr t1,3074   
+sub s0,t0,s0    
+sub s1,t1,s1   
+
+li a7, 1
+mv a0, s0
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+li a7, 1
+mv a0, s1
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+ecall
+
+csrr s1,3074
+csrr s0,3073
+call ff4
+csrr t0,3073    
+csrr t1,3074   
+sub s0,t0,s0    
+sub s1,t1,s1   
+
+li a7, 1
+mv a0, s0
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+li a7, 1
+mv a0, s1
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+ecall
+
+csrr s1,3074
+csrr s0,3073
+call ff5
+csrr t0,3073    
+csrr t1,3074   
+sub s0,t0,s0    
+sub s1,t1,s1   
+
+li a7, 1
+mv a0, s0
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+li a7, 1
+mv a0, s1
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+ecall
+
+csrr s1,3074
+csrr s0,3073
+call ff6
+csrr t0,3073    
+csrr t1,3074   
+sub s0,t0,s0    
+sub s1,t1,s1   
+
+li a7, 1
+mv a0, s0
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+li a7, 1
+mv a0, s1
+ecall
+li a0, '\n'
+li a7, 11
+ecall
+ecall
 li a7, 10
 ecall
 
@@ -125,8 +151,8 @@ ff5:
         slli    a0,a0,2
         ret
 ff6:
-        fcvt.d.w        fa5,a0
         lui     a5,%hi(.LC0)
+        fcvt.d.w        fa5,a0
         fld     fa4,%lo(.LC0)(a5)
         fmul.d  fa5,fa5,fa4
         fcvt.w.d a0,fa5,rtz
