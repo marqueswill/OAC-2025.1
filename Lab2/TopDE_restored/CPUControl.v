83ecce3785375020df2f52bc0ffedd4ffc61ad10
Willyan Marques <willyanmarquesmelo@gmail.com>
2025-05-31 11:45:57 -0300

    new CPU Control outs

--- Lab2/TopDE_restored/CPUControl.v
+++ Lab2/TopDE_restored/CPUControl.v
@@ -1,18 +1,19 @@
 module CPUControl(
 	input  [31:0] iInstruction,
-	//output 		  oRegDst,
 	output 		  oALUSrc,
 	output 		  oMemtoReg,
 	output 		  oRegWrite,
 	output 		  oMemRead,
 	output 		  oMemWrite,
 	output 		  oBranch,
+	output 		  oJal,
+	output 		  oJalr,
 	output [1:0]  oALUOp
 );
 
-reg [7:0] OPCODE;
-reg [7:0] FUNCT7;
-reg [3:0] FUNCT3;
+wire [7:0] OPCODE;
+wire [7:0] FUNCT7;
+wire [3:0] FUNCT3;
 
 assign FUNCT7 = {1'b0, iInstruction[31:25]};
 assign FUNCT3 = {1'b0, iInstruction[14:12]};
@@ -20,12 +21,13 @@ assign OPCODE = {1'b0, iInstruction[6:0]};
 
 wire [7:0] outputs;
 
-
-assign oALUSrc   = outputs[7];
-assign oMemtoReg = outputs[6];
-assign oRegWrite = outputs[5];
-assign oMemRead  = outputs[4];
-assign oMemWrite = outputs[3];
+assign oALUSrc   = outputs[9];
+assign oMemtoReg = outputs[8];
+assign oRegWrite = outputs[7];
+assign oMemRead  = outputs[6];
+assign oMemWrite = outputs[5];
+assign oBranch   = outputs[4];
+assign oBranch   = outputs[3];
 assign oBranch   = outputs[2];
 assign oALUOp    = outputs[1:0];
 
@@ -36,10 +38,10 @@ always @(*) begin
 		IDSLT	 : outputs <= 10'b 0010000010;
 		IDSUB	 : outputs <= 10'b 0010000010;
 		IDADD	 : outputs <= 10'b 0010000010;
-		IDJALR : outputs   <= 10'b 1010000100;
+		IDJALR   : outputs <= 10'b 1010000100;
 		IDBEQ	 : outputs <= 10'b 0000010001;
 		IDSW	 : outputs <= 10'b 1000100000;
-		IDADDI : outputs   <= 10'b 1010000000;
+		IDADDI   : outputs <= 10'b 1010000000;
 		IDLW	 : outputs <= 10'b 1111000000;
 		IDJAL	 : outputs <= 10'b 0010001000;
 		default: outputs   <= ZERO;
