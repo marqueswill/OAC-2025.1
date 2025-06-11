56e9a7e67b3036b08a40cff191b79cd52424a0df
lucasSeabraGomes373 <lucasseabra25@gmail.com>
2025-06-10 21:14:55 -0300

    Update CPUControlMulticiclo.v
    versão funcional da cpu control

--- Lab3/TopDE_restored/CPUControlMulticiclo.v
+++ Lab3/TopDE_restored/CPUControlMulticiclo.v
@@ -33,7 +33,20 @@ always @(posedge iCLK or posedge iRST) begin
 
 	if (iRST)
 		begin // reseta o banco de registradores e pilha
-			regEstado <= 3'b0;
+			regEstado <= 4'd0;
+			EscrevePC    <= 0;
+			EscrevePCCond<= 0;
+			IouD         <= 0;
+			LeMem        <= 0;
+			EscreveMem   <= 0;
+			EscreveIR    <= 0;
+			Mem2Reg      <= 2'b00;
+			OrigPC       <= 0;
+			OrigAULA     <= 2'b00;
+			OrigBULA     <= 2'b00;
+			ALUOp        <= 2'b00;
+			EscreveReg   <= 0;
+			EscrevePCB   <= 0;
 		end
 	else
 	begin
@@ -43,7 +56,7 @@ always @(posedge iCLK or posedge iRST) begin
 				IDSLT	 : direcaoEstado1 <= 4'd6;
 				IDSUB	 : direcaoEstado1 <= 4'd6;
 				IDADD	 : direcaoEstado1 <= 4'd6;
-				//IDJALR : direcaoEstado1 <= ;
+				IDJALR : direcaoEstado1 <= 4'd11;
 				IDBEQ	 : direcaoEstado1 <= 4'd8;
 				IDSW	 : direcaoEstado1 <= 4'd2;
 				IDADDI : direcaoEstado1 <= 4'd10;
@@ -57,18 +70,227 @@ always @(posedge iCLK or posedge iRST) begin
 				default: direcaoEstado2   <= 4'd0;
 			endcase
 		case (regEstado)
-            4'd0: regEstado = 4'd1; // Estado 0
-            4'd1: regEstado = direcaoEstado1; // Estado 1
-            4'd2: regEstado = direcaoEstado2; // Estado 2
-            4'd3: regEstado = 4'd4; // Estado 3
-            4'd4: regEstado = 4'd0; // Estado 4
-            4'd5: regEstado = 4'd0; // Estado 5
-            4'd6: regEstado = 4'd7; // Estado 6
-            4'd7: regEstado = 4'd0; // Estado 7
-            4'd8: regEstado = 4'd0; // Estado 8
-            4'd9: regEstado = 4'd0; // Estado 9
-				4'd9: regEstado = 4'd7; // Estado 10
-            default: regEstado = 5'd0; // Caso padrão (valor inesperado)
+            4'd0:
+				begin
+					regEstado <= 4'd1; // Estado 0
+						EscrevePC<=1;
+						EscrevePCCond<=0;
+						IouD<=0;
+						LeMem<=1;
+						EscreveMem<=0;
+						EscreveIR<=1;
+						Mem2Reg<=2'b00;
+						OrigPC<=0;
+						OrigAULA<=2'b10;
+						OrigBULA<=2'b01;
+						ALUOp<=2'b00;
+						EscreveReg<=0;
+						EscrevePCB<=1;
+				end
+            4'd1:
+				begin
+					 regEstado <= direcaoEstado1; // Estado 1
+						EscrevePC<=0;
+						EscrevePCCond<=0;
+						IouD<=0;
+						LeMem<=0;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b00;
+						OrigPC<=0;
+						OrigAULA<=2'b00;
+						OrigBULA<=2'b10;
+						ALUOp<=2'b00;
+						EscreveReg<=0;
+						EscrevePCB<=0;
+				end
+            4'd2:
+				begin
+					 regEstado <= direcaoEstado2; // Estado 2
+						EscrevePC<=0;
+						EscrevePCCond<=0;
+						IouD<=0;
+						LeMem<=0;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b00;
+						OrigPC<=0;
+						OrigAULA<=2'b01;
+						OrigBULA<=2'b10;
+						ALUOp<=2'b00;
+						EscreveReg<=0;
+						EscrevePCB<=0;
+				end
+            4'd3:
+				begin
+					 regEstado <= 4'd4; // Estado 3
+						EscrevePC<=0;
+						EscrevePCCond<=0;
+						IouD<=1;
+						LeMem<=1;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b00;
+						OrigPC<=0;
+						OrigAULA<=2'b00;
+						OrigBULA<=2'b00;
+						ALUOp<=2'b00;
+						EscreveReg<=0;
+						EscrevePCB<=0;
+				end
+            4'd4:
+				begin
+					 regEstado <= 4'd0; // Estado 4
+						EscrevePC<=0;
+						EscrevePCCond<=0;
+						IouD<=0;
+						LeMem<=0;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b10;
+						OrigPC<=0;
+						OrigAULA<=2'b00;
+						OrigBULA<=2'b00;
+						ALUOp<=2'b00;
+						EscreveReg<=1;
+						EscrevePCB<=0;
+				end
+            4'd5:
+				begin
+					 regEstado <= 4'd0; // Estado 5
+					 EscrevePC<=0;
+						EscrevePCCond<=0;
+						IouD<=1;
+						LeMem<=0;
+						EscreveMem<=1;
+						EscreveIR<=0;
+						Mem2Reg<=2'b00;
+						OrigPC<=0;
+						OrigAULA<=2'b00;
+						OrigBULA<=2'b00;
+						ALUOp<=2'b00;
+						EscreveReg<=0;
+						EscrevePCB<=0;
+				end
+            4'd6:
+				begin
+					 regEstado <= 4'd7; // Estado 6
+					 EscrevePC<=0;
+					 EscrevePCCond<=0;
+						IouD<=0;
+						LeMem<=0;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b00;
+						OrigPC<=0;
+						OrigAULA<=2'b01;
+						OrigBULA<=2'b00;
+						ALUOp<=2'b10;
+						EscreveReg<=0;
+						EscrevePCB<=0;
+				end
+            4'd7:
+				begin
+					 regEstado <= 4'd0; // Estado 7
+					 EscrevePC<=0;
+					 EscrevePCCond<=0;
+						IouD<=0;
+						LeMem<=0;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b00;
+						OrigPC<=0;
+						OrigAULA<=2'b00;
+						OrigBULA<=2'b00;
+						ALUOp<=2'b00;
+						EscreveReg<=1;
+						EscrevePCB<=0;
+				end
+            4'd8:
+				begin
+					 regEstado <= 4'd0; // Estado 8
+					 EscrevePC<=0;
+					 EscrevePCCond<=1;
+						IouD<=0;
+						LeMem<=0;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b00;
+						OrigPC<=1;
+						OrigAULA<=2'b01;
+						OrigBULA<=2'b00;
+						ALUOp<=2'b01;
+						EscreveReg<=0;
+						EscrevePCB<=0;
+				end
+            4'd9:
+				begin
+					 regEstado <= 4'd0; // Estado 9
+					 EscrevePC<=1;
+					 EscrevePCCond<=0;
+						IouD<=0;
+						LeMem<=0;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b01;
+						OrigPC<=1;
+						OrigAULA<=2'b00;
+						OrigBULA<=2'b00;
+						ALUOp<=2'b00;
+						EscreveReg<=1;
+						EscrevePCB<=0;
+				end
+				4'd10:
+				begin
+					 regEstado <= 4'd7; // Estado 10
+					 EscrevePC<=0;
+					 EscrevePCCond<=0;
+						IouD<=0;
+						LeMem<=0;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b00;
+						OrigPC<=0;
+						OrigAULA<=2'b01;
+						OrigBULA<=2'b10;
+						ALUOp<=2'b00;
+						EscreveReg<=0;
+						EscrevePCB<=0;
+				end
+				4'd11:
+				begin
+					 regEstado <= 4'd0; // Estado 11
+					 EscrevePC<=1;
+					 EscrevePCCond<=0;
+						IouD<=0;
+						LeMem<=0;
+						EscreveMem<=0;
+						EscreveIR<=0;
+						Mem2Reg<=2'b01;
+						OrigPC<=0;
+						OrigAULA<=2'b01;
+						OrigBULA<=2'b10;
+						ALUOp<=2'b00;
+						EscreveReg<=1;
+						EscrevePCB<=0;
+				end
+            default: 
+				begin
+					regEstado <= 5'd0; // Caso padrão (valor inesperado)
+					EscrevePC    <= 0;
+					EscrevePCCond<= 0;
+					IouD         <= 0;
+					LeMem        <= 0;
+					EscreveMem   <= 0;
+					EscreveIR    <= 0;
+					Mem2Reg      <= 2'b00;
+					OrigPC       <= 0;
+					OrigAULA     <= 2'b00;
+					OrigBULA     <= 2'b00;
+					ALUOp        <= 2'b00;
+					EscreveReg   <= 0;
+					EscrevePCB   <= 0;
+				end
         endcase
 	end	
 end
